19da11eef95b5d3ad7563e1ab02aec2ae7eb5eee
Александр Манолов <manolov@pyxis.ripcm.com>
2019-07-23 22:50:37 +0300

    fix

--- Figure_2/run_analysis.sh
+++ Figure_2/run_analysis.sh
@@ -3,3 +3,4 @@ python3.5 geneGraph/source/orthofinder_parse.py -i Orthogroups.txt -o graph
 python3.5 geneGraph/source/start_computing.py -i graph.sif -o LF82_B2_graph --reference 50 --save_db graph.db
 grep rRNA lf82_ncbi.gb | grep "\.\." | sed -e 's/complement(//' -e 's/)//' | sed -e 's/.\+\s//' -e 's/\.\./\t/' > lf82.rrna
 grep "tRNA" lf82_ncbi.gb | grep "\.\." | sed -e 's/complement(//' -e 's/)//' -e 's/.\+\s//' -e 's/\.\./\t/' > lf82.trna
+Rscript plot.R
